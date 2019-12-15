0378edb189286a80c2c9356ca91302a526e69e9e
red_and_white <a1457139709@126.com>
2019-12-15 19:01:50 +0800

    添加GitTest

---
+++ src/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @description: git使用
+ * @author red_and_white
+ */
+public class GitTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Using git is a good thing");
+    }
+}
